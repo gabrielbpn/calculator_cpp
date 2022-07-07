2adb6e8bc17f136ae7f4a90d361e74bdeb2b4c3c
GABRIEL NUNES <gabrielbpn0802@gmail.com>
2022-07-07 15:48:10 -0300

    Adicionando mais detalhes ao código e puxando build do cmake

---
+++ build/CMakeFiles/calculadora.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calculadora.dir/calculadora.cpp.o"
+  "calculadora"
+  "calculadora.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calculadora.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- calculadora.cpp
+++ calculadora.cpp
@@ -5,10 +5,11 @@ using namespace std;
 
 int main(void) {
 
-    int num1, num2, soma, sub, multi, div;
-    string string2;
+    int num1, num2;
+    float div, soma, mul, sub;
+    string string2, string3;
 
-    cout << "Olá! Seja bem vindo! Essa é a melhor calculadora que você verá." << endl;
+    cout << "Olá! Seja bem vindo! Essa é a melhor calculadora que você verá na sua vida." << endl;
 
     while(1) {
         cout << "Antes de calcular, veja as notações das operações: \n" << endl;
@@ -18,32 +19,46 @@ int main(void) {
         cout << "Digite o primeiro numero: " << endl;
         cin >> num1;
 
-        cout << "Digite o segundo numero " << endl;
+        cout << "Digite o segundo numero: " << endl;
         cin >> num2;
+
+        div = num1 / num2;
+        mul = num1 * num2;
+        soma = num1 + num2;
+        sub = num1 - num2;
 
         cout << "Qual operação você quer fazer? " << endl;
         cin >> string2;
 
         if (string2 == "*") {
-            printf("%d\n", num1 * num2);
-            break;
+            printf("%.2f\n", mul);
         }
 
         else if (string2 == "-") {
-            printf("%d\n", num1 - num2);
-            break;
+            printf("%.2f\n", sub);
         }
         else if (string2 == "+") {
-            printf("%d\n", num1 + num2);
-            break;
+            printf("%.2f\n", soma);
         }
         else if (string2 == "/") {
-            printf("%d\n", num1 / num2);
-            break;
+            printf("%.2f\n", div);
         }
         else {
-            printf("Vish... Você digitou uma notação errada :(\n Digite novamente.");
+            printf("Vish... Você digitou uma notação errada :(\n");
         }
+
+        cout << "Quer fazer mais alguma conta? (S/N)\n" << endl;
+        cin >> string3;
+        if (string3 == "N" || string3 == "n") {
+            printf("\nVá com Deus!\n");
+            break;
+        }
+
+        else if (string3 != "S" && string3 != "s") {
+            printf("É... Não entendi o que você quis dizer, mas vou assumir que quer ir embora.\nTchau!");
+            break;
+        }   
+
     }
 
     return(0);
